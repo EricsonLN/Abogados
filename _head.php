401e5c7e6c4b7ce081a90817bdb22a592ac413f5
Andy Mariños <andy_mar_rey@outlook.com>
2020-09-11 11:48:52 -0500

    Cambios estilos propiedades etc

--- _head.php
+++ _head.php
@@ -1,30 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous"/>
-    <!-- Resetear Normalize -->
-    
-    <link rel="stylesheet" href="assets/css/normalize.css">
-    <link rel="stylesheet" href="assets/css/color.css">
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/slider.css">
-    <link rel="stylesheet" href="assets/css/especialidades.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/equipo.css">
-    <link rel="stylesheet" href="assets/css/novedades.css">
-    <link rel="stylesheet" href="assets/css/contacto.css">
-    <!-- Mis estilos -->
-    
-    
-    
-
-    
+    <title>Abogados</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous" />
     <!-------SLIDER CARROUSEL===================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
     <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
-    
+    <!-- Mis estilos -->
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/responsive.css">
-    </head>
+</head>
--- _header.php
+++ _header.php
@@ -1,29 +1,25 @@
-<header class="main-header">
-    
-    <img class="main-header__logo" src="assets/img/header-img/LogoL&L.png" alt=""/>
-    
-
-    <nav class="toggle " id="main-menu2">
-        <ul class="menu2" >
-                <li class="menu__item"><a class="menu__link" href="index.php">El estudio </a></li>
-                <li class="menu__item"><a class="menu__link" href="especialidades.php">Especialidades</a></li>
-                <li class="menu__item"><a class="menu__link" href="equipo.php">Equipo</a></li>
-                <li class="menu__item"><a class="menu__link" href="novedades.php">Novedades</a></li>
-                <li class="menu__item"><a class="menu__link" href="contacto.php">Contacto</a></li>
-        </ul>
-    </nav>
-
-    <div class="top-bar" id="icon-menu">
-       <div class="iconmenu" id="icon-menu">
-                <i class="fas fa-bars main-menu__bar"></i>
+<header class="header">
+    <div class="header__container container">
+        <a class="header__logo" href="index.php">
+            <picture>
+                <img src="assets/img/header-img/logo.png" alt="logo de abogados">
+            </picture>
+        </a>
+        <nav class="header__nav">
+            <ul class="main-menu">
+                <li class="main-menu__item"><a href="#" class="main-menu__link">El estudio</a></li>
+                <li class="main-menu__item"><a href="#" class="main-menu__link">Especialidades</a></li>
+                <li class="main-menu__item"><a href="#" class="main-menu__link">Equipo</a></li>
+                <li class="main-menu__item"><a href="#" class="main-menu__link">Novedades</a></li>
+                <li class="main-menu__item"><a href="#" class="main-menu__link">Contacto</a></li>
+            </ul>
+        </nav>
+        <div class="header__information">
+            <a class="header__telephone" href="tel:+51963082509">(51) 963082509</a>
+            <div class="social-networks">
+                <a class="social-networks__item" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a class="social-networks__item" href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            </div>
         </div>
     </div>
-
-    <div class="main-contacto" id="main-contacto">
-        <p class="main-contacto__link">(51) 963082509</p>
-        <i class="fab fa-facebook-f main-contacto__icon"></i>
-        <i class="fab fa-whatsapp main-contacto__icon"></i>
-    </div>   
 </header>
-
-    
